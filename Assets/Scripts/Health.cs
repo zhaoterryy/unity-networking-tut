96497a3790e8f34e22d6f7525922fbb939516204
Terry Zhao <zhao.terryy@gmail.com>
2018-03-27 13:56:55 -0400

    initial commit

---
+++ Assets/Scripts/Health.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+
+public class Health : NetworkBehaviour
+{
+    public const int maxHealth = 100;
+
+    [SyncVar(hook = "OnChangeHealth")]
+    public int currentHealth = maxHealth;
+    public RectTransform healthBar;
+    public bool destroyOnDeath;
+    private NetworkStartPosition[] spawnPoints;
+
+    void Start()
+    {
+        if (isLocalPlayer)
+            spawnPoints = FindObjectsOfType<NetworkStartPosition>();
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (!isServer)
+            return;
+
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            if (destroyOnDeath)
+            {
+                Destroy(gameObject);
+                return;
+            }
+
+            currentHealth = maxHealth;
+            RpcRespawn();
+        }
+    }
+
+    void OnChangeHealth(int health)
+    {
+        healthBar.sizeDelta = new Vector2(health, healthBar.sizeDelta.y);
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        Vector3 spawnPoint = Vector3.zero;
+
+        if (spawnPoints != null && spawnPoints.Length > 0)
+            spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)].transform.position;
+
+        transform.position = spawnPoint;
+    }
+}
